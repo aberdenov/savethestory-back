c9addb7239019e2cfb6a70c81de6034ad2a0b6d5
Alibek Berdenov <souldrinker@bk.ru>
2026-05-25 19:32:33 +0500

    fix cors

--- config/cors.php
+++ config/cors.php
@@ -2,14 +2,21 @@
 
 return [
     'paths' => ['*'],
+
     'allowed_methods' => ['*'],
+
     'allowed_origins' => [
         'https://savethestory.kz',
         'https://www.savethestory.kz',
     ],
+
     'allowed_origins_patterns' => [],
+
     'allowed_headers' => ['*'],
+
     'exposed_headers' => [],
+
     'max_age' => 0,
+
     'supports_credentials' => false,
 ];
--- public/index.php
+++ public/index.php
@@ -1,24 +1,5 @@
 <?php
 
-$allowedOrigins = [
-    'https://savethestory.kz',
-    'https://www.savethestory.kz',
-];
-
-$origin = $_SERVER['HTTP_ORIGIN'] ?? '';
-
-if (in_array($origin, $allowedOrigins, true)) {
-    header("Access-Control-Allow-Origin: {$origin}");
-    header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS');
-    header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, Accept, Origin');
-    header('Access-Control-Max-Age: 86400');
-}
-
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(204);
-    exit;
-}
-
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
