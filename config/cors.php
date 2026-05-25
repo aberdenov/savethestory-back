d7f5a1ea5c4ba5a7708a968336e572b470d1f658
Alibek Berdenov <souldrinker@bk.ru>
2026-05-25 18:51:50 +0500

    cors fix

---
+++ config/cors.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    'paths' => ['api/*', 'auth/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'https://savethestory.kz',
+        'https://www.savethestory.kz',
+    ],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => true,
+
+];
