dce037341793038a6388baba8b51a5ba650d9d6c
Alibek Berdenov <souldrinker@bk.ru>
2026-06-10 21:08:00 +0500

    fix

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -39,9 +39,7 @@ class AuthController extends Controller
 
     public function logout(Request $request)
     {
-        $request->user()
-            ->currentAccessToken() ?
-            ->delete();
+        $request->user()->currentAccessToken()?->delete();
 
         return response()->json(['message' => 'Вы вышли из аккаунта']);
     }
